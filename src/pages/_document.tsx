8523c846f6cfa6523cd66cee4f59cc4950e29bf2
smdhnz <strikerfmy1208@gmail.com>
2023-06-16 15:20:11 +0900

    ♿️ fix _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,6 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 
-type Props = {};
-
-class Document extends NextDocument<Props> {
+class Document extends NextDocument {
   render() {
     return (
       <Html lang="ja">
