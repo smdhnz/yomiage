f94558b6460a9bccf2f903ca15d4c9446f5cc8e4
kato-f <kato-f@etic.co.jp>
2023-08-31 17:51:05 +0900

    style: タイトル

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -3,7 +3,9 @@ import { SignInButton } from "~/components/sign-in-button";
 export default function Page() {
   return (
     <div className="grid gap-6">
-      <h1 className="text-3xl">yomiage - fumiya.dev</h1>
+      <h1 className="text-6xl font-extrabold text-secondary">
+        yomiage - fumiya.dev
+      </h1>
       <SignInButton />
     </div>
   );
