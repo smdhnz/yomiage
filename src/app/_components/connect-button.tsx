225829873579542ad161b247e36ccdf5727b13b5
etic-kato-f <kato-f@etic.co.jp>
2023-10-20 14:47:47 +0900

    prettier formatting

--- src/app/_components/connect-button.tsx
+++ src/app/_components/connect-button.tsx
@@ -65,7 +65,7 @@ export function ConnectButton() {
       setLoading(false);
     });
     newClient.onMessage(async (_channel, user, text, msg) => {
-      const username = msg.userInfo.displayName ?? user
+      const username = msg.userInfo.displayName ?? user;
       toast(user, { description: text });
       console.log(user, text);
       await onMessageHandler(params, username, text);
