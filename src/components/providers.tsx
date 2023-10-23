01857f0656879a16d3283b69dc686af0862ff718
smdhnz <strikerfmy1208@gmail.com>
2023-10-23 14:04:03 +0900

    chore: Toasterにダークモードを反映

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,13 +1,20 @@
 "use client";
 
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useTheme, ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 import { Toaster } from "sonner";
+
+const ToasterProvider = () => {
+  const { theme } = useTheme() as {
+    theme: "light" | "dark" | "system" | undefined;
+  };
+  return <Toaster theme={theme} richColors expand />;
+};
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider {...props}>
-      <Toaster position="top-center" richColors />
+      <ToasterProvider />
       {children}
     </NextThemesProvider>
   );
