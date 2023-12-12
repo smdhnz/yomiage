399628fe1729c5877ccf0623455b8b2c6f4eaf82
smdhnz <strikerfmy1208@gmail.com>
2023-12-12 14:52:50 +0900

    デザイン変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Page() {
   ).map((label, i) => ({ label, value: i }));
 
   return (
-    <div className="flex w-full max-w-xs flex-col items-start gap-4 rounded-xl border p-6 shadow-xl">
+    <div className="flex w-full max-w-xs flex-col items-start gap-4 p-6">
       <ChannelNameInput />
       <div className="flex w-full flex-wrap space-x-2">
         <ConnectButton />
